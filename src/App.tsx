ddcefec8cc4b0c5035bddad4badbed1d48a77a31
halumz <mashuksadman@gmail.com>
2024-01-01 13:14:29 +0600

    feat: building a responsive ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "main aside"`,
+        }}
+      >
+        <GridItem bg="coral" area="nav">
+          nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="gold" area="aside">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem bg="dodgerblue" area="main">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
